cf2d4da1d37bbbc03735afe186aa57886de63adb
Shivamkr522 <work.shivam522@gmail.com>
2022-11-28 10:42:35 +0530

    first task completed

---
+++ src/server/matchesPerYear.js
@@ -0,0 +1,38 @@
+const matches = [];
+const { Console } = require('console');
+const csv = require('csv-parser')
+const fs = require('fs');
+const { mainModule } = require('process');
+const { finished } = require('stream');
+
+fs.createReadStream('../data/matches.csv')
+    .pipe(csv({})).on('data', (data) => matches.push(data))
+    .on('end', () => {
+        matchesPerYear(matches);
+
+    });
+
+
+let findMatchesPlayerPerYear = new Map();
+
+function matchesPerYear(matches) {
+
+    for (var index = 0; index < matches.length; index++) {
+        let value = 1;
+        if (findMatchesPlayerPerYear.has(matches[index].season)) {
+            value = findMatchesPlayerPerYear.get(matches[index].season) + 1;
+            findMatchesPlayerPerYear.set(matches[index].season, value);
+        }
+        else findMatchesPlayerPerYear.set(matches[index].season, value);
+    }
+
+    var data = JSON.stringify(Object.fromEntries(findMatchesPlayerPerYear));
+    fs.writeFile('../public/output/matchesPerYear.json', data, finished)
+    function finished(err) {
+        console.log('File Uploaded');
+    }
+}
+
+
+
+
